51494238b15e98f50a1735834a881906663eee3f
Paul <paul.alexander.rc17@gmail.com>
2026-01-15 08:21:32 -0500

    Commit cambio en la tilde de la e linea 8 en ProductoMapper

--- app/src/main/java/com/example/proyectofinalpoo/logic/ProductoMapper.java
+++ app/src/main/java/com/example/proyectofinalpoo/logic/ProductoMapper.java
@@ -5,7 +5,7 @@ import com.example.proyectofinalpoo.model.Producto;
 
 public class ProductoMapper {
 
-    // Metodo estatico: No necesitas crear una instancia de Mapper para usarlo
+    // Método estatico: No necesitas crear una instancia de Mapper para usarlo
     public static ProductoBase convertirEntidadALogica(Producto entidadProducto, Categoria entidadCategoria) {
 
         // 1. Validaciones de seguridad (por si la BD devuelve nulos)
