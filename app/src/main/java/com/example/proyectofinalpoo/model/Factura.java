3946c7678f1eff897be3b23ffd012d002a884514
alex171215 <alexandercarrion918@gmail.com>
2026-01-20 09:08:37 -0500

    Factura

--- app/src/main/java/com/example/proyectofinalpoo/model/Factura.java
+++ app/src/main/java/com/example/proyectofinalpoo/model/Factura.java
@@ -10,17 +10,34 @@ public class Factura {
     public int id_Factura;
 
     @ColumnInfo(name = "fac_Fecha")
-    public String fecha; // Guardaremos como texto "dd/MM/yyyy"
+    public String fecha;
 
     @ColumnInfo(name = "fac_Total")
     public double total;
 
-    @ColumnInfo(name = "fac_Cliente")
-    public String nombreCliente; // Por simplicidad guardamos el nombre o ID
+    // --- NUEVOS CAMPOS COMPLETOS ---
+    @ColumnInfo(name = "fac_NombreCompleto")
+    public String nombreCliente;
 
-    public Factura(String fecha, double total, String nombreCliente) {
+    @ColumnInfo(name = "fac_Cedula")
+    public String cedula;
+
+    @ColumnInfo(name = "fac_Direccion")
+    public String direccion;
+
+    @ColumnInfo(name = "fac_Celular")
+    public String celular;
+
+    // Constructor VACÍO. Room lo necesita para construir los objetos desde la base de datos.
+    public Factura() {}
+
+    // Constructor para nosotros (para crear facturas nuevas al vender).
+    public Factura(String fecha, double total, String nombreCliente, String cedula, String direccion, String celular) {
         this.fecha = fecha;
         this.total = total;
         this.nombreCliente = nombreCliente;
+        this.cedula = cedula;
+        this.direccion = direccion;
+        this.celular = celular;
     }
 }
