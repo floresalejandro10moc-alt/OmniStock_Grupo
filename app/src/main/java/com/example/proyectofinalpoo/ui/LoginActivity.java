4943f830ed883d4fb53100a7b23bab4ae14c5a4f
Joselyn Cadena <jlcadenac@puce.edu.ec>
2026-01-21 19:03:27 -0500

    Modificación de Login con fuentes

--- app/src/main/java/com/example/proyectofinalpoo/ui/LoginActivity.java
+++ app/src/main/java/com/example/proyectofinalpoo/ui/LoginActivity.java
@@ -75,7 +75,7 @@ public class LoginActivity extends AppCompatActivity {
                         ivBigO.animate()
                                 .scaleX(50f)
                                 .scaleY(50f)
-                                .setDuration(1500)
+                                .setDuration(800)
                                 .withEndAction(() -> irAlMenuPrincipal())
                                 .start();
                     } else {
